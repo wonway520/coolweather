6babfb9e40848f0255c57f882a63d01f5bd7e06f
wonway520 <125385231@qq.com>
2016-04-07 10:09:02 +0800

    新增数据帮助类，以及各表对应的实体类。

---
+++ src/model/Province.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Province {
+	private int id;
+	private String provinceName;
+	private String provinceCode;
+	
+	public void setId(int id){
+		this.id=id;
+	}
+	public int getId(){
+		return id;
+	}
+	public void setProvinceName(String provinceName){
+		this.provinceName=provinceName;
+	}
+	public String getProvinceName(){
+		return provinceName;
+	}
+	public void setProvinceCode(String provinceCode){
+		this.provinceCode=provinceCode;
+	}
+	public String getProvinceCode(){
+		return provinceCode;
+	}
+}
